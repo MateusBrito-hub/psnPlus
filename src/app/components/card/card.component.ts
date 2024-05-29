27a03c92eb0436704efeab929c43beb275ffdd79
Brito <mateusvsbrito6@gmail.com>
2024-05-29 12:59:37 -0300

    feat: finish project

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CardLabelComponent } from './card-label/card-label.component';
+import { CardPriceComponent } from './card-price/card-price.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CardLabelComponent, CardPriceComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+	@Input()
+	gameCover:string =''
+	@Input()
+	gameLabel:string= ''
+	@Input()
+	gameType:string = ''
+	@Input()
+	gamePrice:string = ''
 }
